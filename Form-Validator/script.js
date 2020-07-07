8daed94f10393bd26914091ad88e95b23792dbdf
Rama Sabajo <rsabajo@gmail.com>
2020-07-07 17:24:31 +0200

    finished validator

--- Form-Validator/script.js
+++ Form-Validator/script.js
@@ -4,17 +4,78 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+// Show input error message
 const showError = (input, message) => {
   const formControl = input.parentElement;
   formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+};
+
+//Show succes outline
+const showSucces = input => {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control succes';
+};
+
+// Check email is valid
+const checkEmail = input => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (re.test(input.value.trim())) {
+    showSucces(input);
+  } else {
+    showError(input, 'Please enter a valid email');
+  }
+};
+
+// Check required fields
+const checkRequired = inputArr => {
+  inputArr.forEach(input => {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSucces(input);
+    }
+  });
+};
+
+// Check input length
+const checkLength = (input, min, max) => {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSucces(input);
+  }
+};
+
+// Check passwords match
+const checkPasswordsMatch = (input1, input2) => {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords must match, please try again');
+  }
+};
+
+// Get fieldname
+const getFieldName = input => {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 };
 
 // Event listeners
 form.addEventListener('submit', e => {
   e.preventDefault();
-  if (username.value === '') {
-    showError(username, 'Username is required');
-  } else {
-    showSucces(username);
-  }
+  checkRequired([username, email, password, password2]);
+
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
 });
